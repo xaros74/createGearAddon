cdc3a99b8af52bcd62df1a11f7996b008f6300d3
xaros74 <durettheo74@gmail.com>
2021-10-16 20:29:43 +0200

    now the bug fixe work...

--- src/main/java/com/xaros74/creategearaddon/blocks/AddonCogWheel.java
+++ src/main/java/com/xaros74/creategearaddon/blocks/AddonCogWheel.java
@@ -1,11 +1,21 @@
 package com.xaros74.creategearaddon.blocks;
 
 import com.simibubi.create.content.contraptions.relays.elementary.CogWheelBlock;
+import com.xaros74.creategearaddon.index.AllModTileEntities;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockReader;
 
 public class AddonCogWheel extends CogWheelBlock {
 
 	public AddonCogWheel(boolean large, Properties properties) {
 		super(large, properties);
+	}
+
+	@Override
+	public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+		return AllModTileEntities.getCOG_TILE().create();
 	}
 
 }
--- src/main/java/com/xaros74/creategearaddon/index/AllModTileEntities.java
+++ src/main/java/com/xaros74/creategearaddon/index/AllModTileEntities.java
@@ -7,16 +7,23 @@ import com.xaros74.creategearaddon.CreateGearAddon;
 import com.xaros74.creategearaddon.tiles.CogTile;
 
 public class AllModTileEntities {
-	private static final TileEntityEntry<CogTile> COG_TILE = CreateGearAddon.registrate()
-			.tileEntity("simple_kinetic", CogTile::new).instance(() -> SingleRotatingInstance::new)
-			.validBlocks(AllModBlocks.OAK_COGWHEEL, AllModBlocks.ACACIA_COGWHEEL, AllModBlocks.BIRCH_COGWHEEL,
-					AllModBlocks.CRIMSON_COGWHEEL, AllModBlocks.DARK_OAK_COGWHEEL, AllModBlocks.JUNGLE_COGWHEEL,
-					AllModBlocks.WARPED_COGWHEEL, AllModBlocks.LARGE_OAK_COGWHEEL, AllModBlocks.LARGE_BIRCH_COGWHEEL,
-					AllModBlocks.LARGE_ACACIA_COGWHEEL, AllModBlocks.LARGE_DARK_OAK_COGWHEEL,
-					AllModBlocks.LARGE_CRIMSON_COGWHEEL, AllModBlocks.LARGE_WARPED_COGWHEEL)
-			.renderer(() -> KineticTileEntityRenderer::new).register();
+	private static TileEntityEntry<CogTile> COG_TILE;
 
 	public static void register() {
+		COG_TILE = CreateGearAddon.registrate().tileEntity("simple_kinetic", CogTile::new)
+				.instance(() -> SingleRotatingInstance::new)
+				.validBlocks(AllModBlocks.OAK_COGWHEEL, AllModBlocks.ACACIA_COGWHEEL, AllModBlocks.BIRCH_COGWHEEL,
+						AllModBlocks.CRIMSON_COGWHEEL, AllModBlocks.DARK_OAK_COGWHEEL, AllModBlocks.JUNGLE_COGWHEEL,
+						AllModBlocks.WARPED_COGWHEEL, AllModBlocks.LARGE_OAK_COGWHEEL,
+						AllModBlocks.LARGE_BIRCH_COGWHEEL, AllModBlocks.LARGE_ACACIA_COGWHEEL,
+						AllModBlocks.LARGE_DARK_OAK_COGWHEEL, AllModBlocks.LARGE_CRIMSON_COGWHEEL,
+						AllModBlocks.LARGE_WARPED_COGWHEEL)
+				.renderer(() -> KineticTileEntityRenderer::new).register();
+
+	}
+
+	public static TileEntityEntry<CogTile> getCOG_TILE() {
+		return COG_TILE;
 	}
 
 }
