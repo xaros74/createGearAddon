beff7c4aa473844ada5914978faa1e731568fa29
xaros74 <durettheo74@gmail.com>
2022-07-14 21:59:42 +0200

    add bracket behavior

--- src/main/java/com/xaros74/creategearaddon/tiles/CogTile.java
+++ src/main/java/com/xaros74/creategearaddon/tiles/CogTile.java
@@ -2,7 +2,10 @@ package com.xaros74.creategearaddon.tiles;
 
 import java.util.List;
 
+import com.simibubi.create.content.contraptions.relays.elementary.AbstractShaftBlock;
+import com.simibubi.create.content.contraptions.relays.elementary.BracketedTileEntityBehaviour;
 import com.simibubi.create.content.contraptions.relays.elementary.SimpleKineticTileEntity;
+import com.simibubi.create.foundation.advancement.AllTriggers;
 import com.simibubi.create.foundation.tileEntity.TileEntityBehaviour;
 
 import net.minecraft.tileentity.TileEntityType;
@@ -14,8 +17,10 @@ public class CogTile extends SimpleKineticTileEntity {
 	}
 	
 	@Override
-    public void addBehaviours(List<TileEntityBehaviour> behaviours) {
-        // dont add bracket behavior
-    }
+	public void addBehaviours(List<TileEntityBehaviour> behaviours) {
+		behaviours.add(new BracketedTileEntityBehaviour(this, state -> state.getBlock() instanceof AbstractShaftBlock)
+			.withTrigger(state -> AllTriggers.BRACKET_APPLY_TRIGGER.constructTriggerFor(state.getBlock())));
+		super.addBehaviours(behaviours);
+	}
 
 }
